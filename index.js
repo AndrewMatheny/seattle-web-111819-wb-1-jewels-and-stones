a0b932ec5561962b14900794d0ecad6d8435d08f
AndrewMatheny <github email address>
2020-01-30 13:15:13 -0800

    basic solution done

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("hey look, some code!");
-  jewelsAndStones();
+  jewelsAndStones("aB", "aBBAABBBAASKFDSL");
 });
 
 jewelsAndStones = (jewels, stones) => {
   console.log("checking which stones are jewels...");
-  //code your answer here!!!
+  let jewelArray = jewels.split("")
+  let stoneArray = stones.split("")
+  let counter = 0
+  for(i = 0; i<stoneArray.length; i++) {
+    for(j = 0; j<jewelArray.length; j++) {
+      if(stoneArray[i] === jewelArray[j]) {
+        counter++
+      }
+    }
+  }
+  console.log(counter)
+  return(counter)
 };
